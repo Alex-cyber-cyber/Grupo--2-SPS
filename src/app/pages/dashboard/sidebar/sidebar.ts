92e47863a8a57819ceb9b9a242e4309f9f06140d
NathalieZD <167153017+NathalieZD@users.noreply.github.com>
2026-02-15 20:25:38 -0600

    Actualizando diseño

--- src/app/pages/dashboard/sidebar/sidebar.ts
+++ src/app/pages/dashboard/sidebar/sidebar.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../auth/services/auth.service';
+import { UiService } from '../../../services/ui.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,17 +11,30 @@ import { AuthService } from '../../../auth/services/auth.service';
   styleUrls: ['./sidebar.css'],
 })
 export class SidebarComponent {
-  isCollapsed = false;
 
-  toggleSidebar() {
-    this.isCollapsed = !this.isCollapsed;
+  isOpen = false;
+
+  
+  private ui = inject(UiService);
+  private router = inject(Router);
+  private authService = inject(AuthService);
+
+  constructor() {
+    
+    this.ui.sidebarOpen$.subscribe(open => {
+      this.isOpen = open;
+    });
   }
-
-  router = inject(Router);
-  authService = inject(AuthService);
 
   async logout() {
     await this.authService.logout();
+    this.ui.closeSidebar(); 
     window.location.reload();
+  }
+
+  closeOnMobile() {
+    if (window.innerWidth < 768) {
+      this.ui.closeSidebar();
+    }
   }
 }
--- src/app/pages/dashboard/topbar/topbar.ts
+++ src/app/pages/dashboard/topbar/topbar.ts
@@ -1,7 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { AuthService } from '../../../auth/services/auth.service';
 import { AsyncPipe } from '@angular/common';
-
+import { UiService } from '../../../services/ui.service';
 @Component({
   selector: 'app-topbar',
   standalone: true,
@@ -10,5 +10,11 @@ import { AsyncPipe } from '@angular/common';
   imports: [AsyncPipe],
 })
 export class TopbarComponent {
+
+  private ui = inject(UiService);
   authService = inject(AuthService);
+
+  toggleSidebar() {
+    this.ui.toggleSidebar();
+  }
 }
